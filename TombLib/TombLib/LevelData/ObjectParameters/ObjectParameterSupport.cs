b4778c961e3137bf575b54af85ab0b77cecb7ea6
Christoph Summerer <info@trlevel.de>
2026-06-05 11:46:22 +0200

    Allow object parameters for OCB objects outside TEN

--- TombLib/TombLib/LevelData/ObjectParameters/ObjectParameterSupport.cs
+++ TombLib/TombLib/LevelData/ObjectParameters/ObjectParameterSupport.cs
@@ -6,6 +6,22 @@ namespace TombLib.LevelData.ObjectParameters
     {
         public static bool IsSupported(Level level)
         {
+            return level != null;
+        }
+
+        public static bool IsSupported(Level level, ObjectInstance instance)
+        {
+            if (level == null || instance == null)
+                return false;
+
+            if (level.IsTombEngine)
+                return true;
+
+            return instance is ItemInstance;
+        }
+
+        public static bool SupportsRuntimeParameters(Level level)
+        {
             return level != null && level.IsTombEngine;
         }
     }
